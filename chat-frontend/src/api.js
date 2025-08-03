14aef402d7a27a89eaf2ea5bc5d822627aeb326d
HAMDAN SIBDOO ZAKARIA <160386088+Hamdan-gh@users.noreply.github.com>
2025-08-03 19:04:15 +0000

    Update api.js
    at base url

--- chat-frontend/src/api.js
+++ chat-frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://live-chat-app-vw20.onrender.com/api', // ✅ no double /api/api issue
+  baseURL: 'https://live-chat-app-vw20.onrender.com', // ✅ no double /api/api issue
   withCredentials: true, // optional if you're using cookies/auth
 });
 
